11e28c57d010690f4f2e8bf13eb7d99138f59acb
angelicaputri <angelica.amartya01@gmail.com>
2021-05-29 21:21:47 +0700

    sukses

---
+++ app/src/main/java/com/example/databaselokal/view/MainContact.java
@@ -0,0 +1,28 @@
+package com.example.databaselokal.view;
+
+import android.view.View;
+
+import com.example.databaselokal.entity.AppDatabase;
+import com.example.databaselokal.entity.DataKampus;
+
+import java.util.List;
+
+public interface MainContact {
+    //untuk kodingan activity
+    interface view extends View.OnClickListener{
+        void successAdd();
+        void successDelete();
+        void resetForm();
+        void getData(List<DataKampus> list);
+        void editData(DataKampus item);
+        void deleteData(DataKampus item);
+    }
+
+    //kodingan untuk database
+    interface presenter{
+        void insertData(String nama, String alamat, int jumlah, AppDatabase database);
+        void readData(AppDatabase database);
+        void editData(String nama, String alamat, int jumlah, int id, AppDatabase database);
+        void deleteData(DataKampus dataKampus, AppDatabase database);
+    }
+}
